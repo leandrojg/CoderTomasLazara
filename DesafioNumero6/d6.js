cfc9625c286f1f0381bb997e66d10890cc6cd443
topo <tomasflazara@gmail.com>
2021-08-08 15:45:12 -0300

    Desafio numero 6

---
+++ DesafioNumero6/d6.js
@@ -0,0 +1,95 @@
+const fs = require("fs");
+const ArrayProductos = [];
+
+// implementar programa que contenga una clase llamada Archivo que reciba
+//  el nombre del archivo con el que va a trabajar e implemente los métodos leer,
+//  guardar, borrar.
+class Archivo 
+{
+  constructor(archivo)
+  {
+    this.archivo = archivo;
+  }
+// Utilizar guardar para incorporar productos al archivo "productos.txt"
+// La función guardar incorporará al producto un id, 
+// el cual se obtendrá de la longitud total del array actual más 1.
+  async Guardar(producto)
+  {
+    try
+    {
+      ArrayProductos.push(({ ...producto, id: ArrayProductos.length + 1 }))
+      try
+      {
+         await fs.promises.writeFile(
+          this.archivo,
+          JSON.stringify(ArrayProductos, null, '\t')
+        );
+      }
+      catch(err)
+      {
+        throw new Error(err);
+      }
+    }
+    catch(err)
+    {
+      console.log(err)
+    }
+  }
+// Con el método leer se mostrará en consola el listado total 
+// (si el archivo existe) como un array de productos.
+  Leer() 
+  {
+    let comprobacion = fs.existsSync(this.archivo);
+
+     if(comprobacion)
+     {
+       fs.promises.readFile(this.archivo, "utf-8")
+       .then(data=>{ 
+         console.log(data)
+       })
+     }
+     else
+     {
+       console.log("archivo inexistente")
+     }
+  }
+//El método borrar elimina el archivo completo.
+// Implementar el manejo de archivos con el módulo fs de node.js,
+//  utilizando promesas con async await y manejo de errores.
+  async Borrar()
+  {
+    try
+    {
+        await fs.promises.unlink(this.archivo)
+    }
+    catch(err)
+    {
+      console.log(err)
+    }
+  }
+}
+
+const random = (min, max)=> { 
+  let rangoAleatorio = Math.floor(Math.random() * (max-min))
+  return rangoAleatorio;
+}
+
+let obProducto1 = {
+  producto: `Banana`,
+  precio: `${random(0.00, 9999.99)}`,
+  thumbnail: `Foto: ${random(1,10)}`
+}
+
+let obProducto2 = {
+  producto: `Tomate`,
+  precio: `${random(0.00, 9999.99)}`,
+  thumbnail: `Foto: ${random(1,10)}`
+}
+
+let nuevoArchivo = new Archivo("productos.txt");
+nuevoArchivo.Guardar(obProducto1);
+nuevoArchivo.Guardar(obProducto2);
+nuevoArchivo.Leer();
+
+//dejo la funcion borrar en un timeOut para remarcar la diferencia
+setTimeout(()=>nuevoArchivo.Borrar(), 5000)
