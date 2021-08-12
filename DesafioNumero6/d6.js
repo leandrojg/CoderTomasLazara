e09d46941c4650f370909d75b5e9c17a52ecfcd6
topo <tomasflazara@gmail.com>
2021-08-11 23:02:53 -0300

    correcciones

--- DesafioNumero6/d6.js
+++ DesafioNumero6/d6.js
@@ -13,51 +13,35 @@ class Archivo
 // Utilizar guardar para incorporar productos al archivo "productos.txt"
 // La función guardar incorporará al producto un id, 
 // el cual se obtendrá de la longitud total del array actual más 1.
-  async Guardar(producto)
-  {
-    let comprobacion =  fs.existsSync(this.archivo);
-
-    if(comprobacion)
+async Guardar(producto) {
+    try {
+      ArrayProductos.push({ ...producto, id: ArrayProductos.length + 1 });
+      await fs.promises.writeFile(
+        this.archivo,
+        JSON.stringify(ArrayProductos, null, "\t")
+      );
+    }
+    catch (err) 
     {
-      try
-      {
-          ArrayProductos.push(({ ...producto, id: ArrayProductos.length + 1 }))
-          try
-          {
-            await fs.promises.writeFile(
-              this.archivo,
-              JSON.stringify(ArrayProductos, null, '\t')
-            );
-          }
-          catch(err)
-          {
-            throw new Error(err);
-          }
-      }
-      catch(err)
-      {
-        console.log(err)
-      }
-  }
-  }
-// Con el método leer se mostrará en consola el listado total 
+      console.log(err);
+    }
+}
+// Con el método leer se mostrará en consola el listado total
 // (si el archivo existe) como un array de productos.
-  async Leer() 
-  {
-    let comprobacion =  fs.existsSync(this.archivo);
-
-     if(comprobacion)
-     {
-       await fs.promises.readFile(this.archivo, "utf-8")
-       .then(data=>{ 
-         console.log(data)
-       })
-     }
-     else
-     {
-       console.log(ArrayProductos)
-     }
+async Leer() {
+  let comprobacion = fs.existsSync(this.archivo);
+  if (comprobacion)
+   {
+    await fs.promises.readFile(this.archivo, "utf-8").then((data) => {
+      console.log(data);
+    });
   }
+   else 
+   {
+    console.log(ArrayProductos);
+   }
+}
+
 //El método borrar elimina el archivo completo.
 // Implementar el manejo de archivos con el módulo fs de node.js,
 //  utilizando promesas con async await y manejo de errores.
@@ -100,9 +84,9 @@ let obProducto2 = {
 }
 
 let nuevoArchivo = new Archivo("productos.txt");
+nuevoArchivo.Leer();
 nuevoArchivo.Guardar(obProducto1);
 nuevoArchivo.Guardar(obProducto2);
-nuevoArchivo.Leer();
 
 //dejo la funcion borrar en un timeOut para remarcar la diferencia
-setTimeout(()=>nuevoArchivo.Borrar(), 5000)
+//setTimeout(()=>nuevoArchivo.Borrar(), 5000)
